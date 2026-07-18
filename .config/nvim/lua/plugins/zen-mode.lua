739e7768743e155ebeca0fb58cd0a31aa029a9bd
Aliaksandr Kulba <tunet000@gmail.com>
2026-07-18 22:06:02 +0300

    refacotr: zen-mode config

--- .config/nvim/lua/plugins/zen-mode.lua
+++ .config/nvim/lua/plugins/zen-mode.lua
@@ -6,21 +6,9 @@ return {
         width = 120,
         options = {
           wrap = true,
+	  laststatus = 3,
         },
       },
-      plugins = {
-        options = {
-          enabled = true,
-          statusline = false,
-        },
-      },
-      on_open = function(win)
-        -- Wait a microsecond for the window to settle, then force the statusline active
-        vim.schedule(function()
-          vim.opt.laststatus = 3
-          vim.wo[win].statusline = "%!v:lua.MiniStatusline.active()"
-        end)
-      end,
     },
     keys = {
       { "<leader>z", "<cmd>ZenMode<cr>", desc = "Toggle Zen Mode (Center Code)" }
